0024b0f6eb86f5f36e62ed88deefa5c6e3fcaa01
Toxa-p07a1330 <p07a1330@yandex.ru>
2021-02-13 23:00:44 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import EqualizerPanel from "./EqualizerPanel";
 import FileSelector from "./FileSelector";
 import {useContext, useState} from 'react'
 import {UserContext} from "./Context";
+import Controls from "./Controls";
 
 function App() {
     const context = useContext(UserContext);
-    const [wasStarted, setStarted] = useState();
-    const [wasStopped, setStopped] = useState();
-    const [wasFileLoaded, setFileLoaded] = useState(false);
 
     let setAudioCtx = ()=>{
         let data = context.data;
@@ -20,47 +18,12 @@ function App() {
     if (!context.data.audioCtx)
         setAudioCtx();
 
-    let startPlaying = ()=>{
-        let nodeWithEffect = context.data.getNodeWithEffect(context.data.sourceNode)
-        let nodeWithEqualizerAndEffects = context.data.getNodeWithEqualization(nodeWithEffect);
-        nodeWithEqualizerAndEffects.connect(context.data.audioCtx.destination)
-        context.data.sourceNode.start();
-        setStarted(true);
-    }
-    let stopPlaying = ()=>{
-        if (context.data.sourceNode) {
-            context.data.sourceNode.stop();
-            setStopped(true);
-        }
-
-    }
-    let reinit = ()=>{
-        setAudioCtx();
-        context.data.setSourceNode();
-        setStopped(false);
-        setStarted(false);
-        setFileLoaded(false);
-    }
-    document.addEventListener("fileLoaded", ()=>{
-        if (!wasFileLoaded) {
-            setFileLoaded(true);
-        }
-    })
     return (
         <div className="App">
             <FileSelector/>
             <EffectsPanel/>
             <EqualizerPanel/>
-            <div></div>
-            <button onClick={startPlaying} id={"start"} disabled={wasStarted || ! wasFileLoaded}>
-                Start
-            </button>
-            <button onClick={stopPlaying} id={"stop"} disabled={!(wasStarted && !wasStopped)}>
-                Stop
-            </button>
-            <button onClick={reinit} id={"reinit"} disabled={!wasStopped}>
-            Reinit
-            </button>
+            <Controls/>
         </div>
     );
 }
---
+++ src/Controls.js
@@ -0,0 +1,53 @@
+import './App.css';
+import {useContext, useState} from 'react'
+import {UserContext} from "./Context";
+
+
+let Controls = ()=>{
+    const context = useContext(UserContext);
+    const [wasStarted, setStarted] = useState();
+    const [wasStopped, setStopped] = useState();
+    const [wasFileLoaded, setFileLoaded] = useState(false);
+
+    let startPlaying = ()=>{
+        let nodeWithEffect = context.data.getNodeWithEffect(context.data.sourceNode)
+        let nodeWithEqualizerAndEffects = context.data.getNodeWithEqualization(nodeWithEffect);
+       // let nodeWithGraphicsAndAnalyser = context.data.getNodeWithAnalyser(nodeWithEqualizerAndEffects);
+        nodeWithEqualizerAndEffects.connect(context.data.audioCtx.destination)
+        context.data.sourceNode.start();
+        setStarted(true);
+    }
+    let stopPlaying = ()=>{
+        if (context.data.sourceNode) {
+            context.data.sourceNode.stop();
+            setStopped(true);
+        }
+
+    }
+    let reinit = ()=>{
+        context.data.setSourceNode();
+        setStopped(false);
+        setStarted(false);
+        setFileLoaded(false);
+    }
+    document.addEventListener("fileLoaded", ()=>{
+        if (!wasFileLoaded) {
+            setFileLoaded(true);
+        }
+    })
+    return (
+        <div className="App">
+            <button onClick={startPlaying} id={"start"} disabled={wasStarted || ! wasFileLoaded}>
+                Start
+            </button>
+            <button onClick={stopPlaying} id={"stop"} disabled={!(wasStarted && !wasStopped)}>
+                Stop
+            </button>
+            <button onClick={reinit} id={"reinit"} disabled={!wasStopped}>
+                Reinit
+            </button>
+        </div>
+    );
+}
+
+export default Controls;
